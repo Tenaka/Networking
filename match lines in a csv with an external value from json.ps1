1385ffa09ae7ec96daf7ff42901ab7d3b5dc2839
Tenaka <86342641+Tenaka@users.noreply.github.com>
2023-05-02 16:46:01 +0100

    Add files via upload

---
+++ match lines in a csv with an external value from json.ps1
@@ -0,0 +1,54 @@
+﻿<#
+An example of a csv file mappings between CIDR and Subnet eg 24,255.255.255.0 
+The JSON input uses the subnet eg 255.255.255.0
+However the New-NetIPAddress -Prefixlength requires '24'
+
+This example shows how to match lines in a csv with an external value
+
+#>
+
+#PWD for ISE or Powershell 
+if($psise -ne $null)
+    {
+        $ISEPath = $psise.CurrentFile.FullPath
+        $ISEDisp = $psise.CurrentFile.DisplayName.Replace("*","")
+        $Pwdir = $ISEPath.TrimEnd("$ISEDisp")
+    }
+    else
+    {
+        $Pwdir = split-path -parent $MyInvocation.MyCommand.Path
+    }
+
+#JSON File for DCPromo
+$dcPromoJson = "DCPromov.0.1.json"
+
+#Lists of Subnets and their CIDRs
+$dcPromoSubnet = "Subnet.csv"
+
+#Import JSON
+$gtDCPromoJ = Get-Content -raw -Path "$($Pwdir)\$($dcPromoJson)" | ConvertFrom-Json
+
+#Import csv of Subnets 
+[array]$gtSubnet = Import-Csv -Path "$($Pwdir)\$($dcPromoSubnet)"
+
+#Var the subnet value eg 255.255.255.0
+$PDC_Subnet = $gtDCPromoJ.FirstDC.Subnet
+
+#Subnet to CIDR conversion table - Match the 255.255.255.0 to the CIDR in the table
+foreach($lnSubnet in $gtSubnet)
+    {
+        if ($lnSubnet.mask -cmatch $PDC_Subnet)
+            {
+                $dcSubnet = $lnSubnet.cidr
+            }
+    }
+#
+New-NetIPAddress -InterfaceAlias $gtNetAdpap.Name `
+                 -IPAddress $PDC_IP                       `
+                 -AddressFamily IPv4                      `
+                 -PrefixLength $dcSubnet                         `
+                 -DefaultGateway $PDC_Route
+        
+#Set DNS Server                 
+Set-DnsClientServerAddress -ServerAddresses $PDC_IP -InterfaceAlias $intAlias
+
---
+++ setStaticIP.ps1
@@ -0,0 +1,29 @@
+﻿########################################################################################
+###################################  SET STATIC IP  ####################################
+########################################################################################
+
+Write-Host "Set a Static IP Address" -ForegroundColor yellow
+$gNetAdp = Get-NetAdapter | where {$_.Status -eq "up"}
+$intAlias = $gNetAdp.InterfaceAlias
+
+$gNetIPC = Get-NetIPConfiguration -InterfaceAlias $gNetAdp.Name
+$IPAddress = $gNetIPC.IPv4Address.ipaddress
+$DHCPRouter = $gNetIPC.IPv4DefaultGateway.nexthop
+$dnsAddress = $gNetIPC.dnsserver.serveraddresses
+
+$gNetIPC | Remove-NetIPAddress -Confirm:$false
+$gNetIPC.IPv4DefaultGateway |Remove-NetRoute -Confirm:$false -ErrorAction SilentlyContinue
+
+$IPAddress = Read-Host "Enter the Static IP"
+$DefGate = Read-Host "Enter the Default Gateway eg 192.168.0.254"
+$dnsServer = Read-Host "Enter DNS IP(s) eg 192.168.0.22 or 192.168.0.22,192.168.0.23"
+$dnsName = Read-Host "Enter an FQDN eg Contoso.net"
+    
+#Set Static IP
+New-NetIPAddress -InterfaceAlias $gNetAdp.Name `
+                 -IPAddress $IPAddress `
+                 -AddressFamily IPv4 `
+                 -PrefixLength 24 `
+                 -DefaultGateway $DefGate
+#Set DNS Server                 
+Set-DnsClientServerAddress -ServerAddresses $dnsServer -InterfaceAlias $intAlias
