d9bc9193ea204a24203b0f2228744ac12d2955db
Tenaka <86342641+Tenaka@users.noreply.github.com>
2023-10-12 06:52:13 +0100

    Add files via upload

---
+++ adds seperator to list of mac addresses.ps1
@@ -0,0 +1,17 @@
+﻿#Path to files
+$macPath = "C:\Users\Administrator\Desktop\"
+
+#Input file
+$gtMacList = (Get-Content "$($macPath)\maclist.txt").Replace(" ","")
+
+#tests for if output file exists
+if (-not $macPath){new-item "$($macPath)\ouptut.txt" -ItemType File }
+
+#does the biz
+foreach($macitem in $gtMacList)
+    {
+        $macJoin = ($macitem.ToLower() -split "(..)"  | Where-Object {$_}) -join "-"
+        Write-Host $macJoin -ForegroundColor Green
+        $macJoin | Out-File "$($macPath)\ouptut.txt" -Append    
+    }
+
